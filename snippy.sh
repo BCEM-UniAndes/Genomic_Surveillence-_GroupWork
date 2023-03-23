f4445aa7f169779010284b11599b3d8f0e761e0c
Nathalia Portilla <67386652+portillanath@users.noreply.github.com>
2023-03-23 11:03:50 -0500

    Add files via upload
    spades assembly script

---
+++ snippy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#This script for alignment from trimmed fastq.gz
+function docker_run() { docker run --rm=True -u $(id -u):$(id -g) -v $(pwd):/data "$@" ;}
+wordir=/home/bioinfo-sanger/Group_work/species/s.pneumo/results_fastqc_gz/trimmed_results/results_trimmed_gz
+cd $wordir
+for i in $(ls *_1.trimmed.fastq.gz); do
+NAME=$(basename $i _1.trimmed.fastq.gz)
+echo "$NAME"
+j="${NAME}_1.trimmed.fastq.gz"
+echo "$j"
+k="${NAME}_2.trimmed.fastq.gz"
+echo "$k"
+docker_run staphb/snippy snippy --outdir ${NAME}_GPSC46_folder --R1 $j --R2 $k --ref Reference_sequence_GPSC46.fa --cpus 2 --ram 2 --force --quiet;
+done
