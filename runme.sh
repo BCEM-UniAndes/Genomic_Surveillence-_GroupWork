357629bc52022be9fef50dd81f5b762e695b9bc5
Nathalia Portilla <67386652+portillanath@users.noreply.github.com>
2023-03-23 11:15:32 -0500

    Add files via upload
    Output script for snippy alignment

---
+++ runme.sh
@@ -0,0 +1,21 @@
+snippy --outdir 'pneumoniae10' --R1 '/data/pneumoniae10_1.trimmed.fastq.gz' --R2 '/data/pneumoniae10_2.trimmed.fastq.gz' --ref Reference_sequence_GPSC46.fa --cpus 2
+snippy --outdir 'pneumoniae11' --R1 '/data/pneumoniae11_1.trimmed.fastq.gz' --R2 '/data/pneumoniae11_2.trimmed.fastq.gz' --ref Reference_sequence_GPSC46.fa --cpus 2
+snippy --outdir 'pneumoniae12' --R1 '/data/pneumoniae12_1.trimmed.fastq.gz' --R2 '/data/pneumoniae12_2.trimmed.fastq.gz' --ref Reference_sequence_GPSC46.fa --cpus 2
+snippy --outdir 'pneumoniae13' --R1 '/data/pneumoniae13_1.trimmed.fastq.gz' --R2 '/data/pneumoniae13_2.trimmed.fastq.gz' --ref Reference_sequence_GPSC46.fa --cpus 2
+snippy --outdir 'pneumoniae14' --R1 '/data/pneumoniae14_1.trimmed.fastq.gz' --R2 '/data/pneumoniae14_2.trimmed.fastq.gz' --ref Reference_sequence_GPSC46.fa --cpus 2
+snippy --outdir 'pneumoniae15' --R1 '/data/pneumoniae15_1.trimmed.fastq.gz' --R2 '/data/pneumoniae15_2.trimmed.fastq.gz' --ref Reference_sequence_GPSC46.fa --cpus 2
+snippy --outdir 'pneumoniae16' --R1 '/data/pneumoniae16_1.trimmed.fastq.gz' --R2 '/data/pneumoniae16_2.trimmed.fastq.gz' --ref Reference_sequence_GPSC46.fa --cpus 2
+snippy --outdir 'pneumoniae17' --R1 '/data/pneumoniae17_1.trimmed.fastq.gz' --R2 '/data/pneumoniae17_2.trimmed.fastq.gz' --ref Reference_sequence_GPSC46.fa --cpus 2
+snippy --outdir 'pneumoniae18' --R1 '/data/pneumoniae18_1.trimmed.fastq.gz' --R2 '/data/pneumoniae18_2.trimmed.fastq.gz' --ref Reference_sequence_GPSC46.fa --cpus 2
+snippy --outdir 'pneumoniae19' --R1 '/data/pneumoniae19_1.trimmed.fastq.gz' --R2 '/data/pneumoniae19_2.trimmed.fastq.gz' --ref Reference_sequence_GPSC46.fa --cpus 2
+snippy --outdir 'pneumoniae1' --R1 '/data/pneumoniae1_1.trimmed.fastq.gz' --R2 '/data/pneumoniae1_2.trimmed.fastq.gz' --ref Reference_sequence_GPSC46.fa --cpus 2
+snippy --outdir 'pneumoniae20' --R1 '/data/pneumoniae20_1.trimmed.fastq.gz' --R2 '/data/pneumoniae20_2.trimmed.fastq.gz' --ref Reference_sequence_GPSC46.fa --cpus 2
+snippy --outdir 'pneumoniae2' --R1 '/data/pneumoniae2_1.trimmed.fastq.gz' --R2 '/data/pneumoniae2_2.trimmed.fastq.gz' --ref Reference_sequence_GPSC46.fa --cpus 2
+snippy --outdir 'pneumoniae3' --R1 '/data/pneumoniae3_1.trimmed.fastq.gz' --R2 '/data/pneumoniae3_2.trimmed.fastq.gz' --ref Reference_sequence_GPSC46.fa --cpus 2
+snippy --outdir 'pneumoniae4' --R1 '/data/pneumoniae4_1.trimmed.fastq.gz' --R2 '/data/pneumoniae4_2.trimmed.fastq.gz' --ref Reference_sequence_GPSC46.fa --cpus 2
+snippy --outdir 'pneumoniae5' --R1 '/data/pneumoniae5_1.trimmed.fastq.gz' --R2 '/data/pneumoniae5_2.trimmed.fastq.gz' --ref Reference_sequence_GPSC46.fa --cpus 2
+snippy --outdir 'pneumoniae6' --R1 '/data/pneumoniae6_1.trimmed.fastq.gz' --R2 '/data/pneumoniae6_2.trimmed.fastq.gz' --ref Reference_sequence_GPSC46.fa --cpus 2
+snippy --outdir 'pneumoniae7' --R1 '/data/pneumoniae7_1.trimmed.fastq.gz' --R2 '/data/pneumoniae7_2.trimmed.fastq.gz' --ref Reference_sequence_GPSC46.fa --cpus 2
+snippy --outdir 'pneumoniae8' --R1 '/data/pneumoniae8_1.trimmed.fastq.gz' --R2 '/data/pneumoniae8_2.trimmed.fastq.gz' --ref Reference_sequence_GPSC46.fa --cpus 2
+snippy --outdir 'pneumoniae9' --R1 '/data/pneumoniae9_1.trimmed.fastq.gz' --R2 '/data/pneumoniae9_2.trimmed.fastq.gz' --ref Reference_sequence_GPSC46.fa --cpus 2
+snippy-core --ref 'pneumoniae1/ref.fa' pneumoniae1 pneumoniae10 pneumoniae11 pneumoniae12 pneumoniae13 pneumoniae14 pneumoniae15 pneumoniae16 pneumoniae17 pneumoniae18 pneumoniae19 pneumoniae2 pneumoniae20 pneumoniae3 pneumoniae4 pneumoniae5 pneumoniae6 pneumoniae7 pneumoniae8 pneumoniae9
